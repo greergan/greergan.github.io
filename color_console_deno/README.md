8095455c180a9b4cb752c3545a1b4bd7f8d10fda
Jeff Greer <greergan@gmail.com>
2023-03-04 15:37:33 -0800

    readded color console

---
+++ color_console_deno/README.md
@@ -0,0 +1,2 @@
+# color_console_deno
+A colorized console for [Deno](https://deno.land/)
---
+++ color_console_deno/deps.ts
@@ -0,0 +1,1 @@
+export { green, magenta, red, blue, brightGreen, bold, yellow } from "https://deno.land/std@0.133.0/fmt/colors.ts";
