ad308374ed71601bf63b999895605fcbe9bb2bd4
Jeff Greer <greergan@gmail.com>
2023-03-22 11:58:14 -0700

    moved global declaration of SlimConsole to slim.console

--- slim_global.ts
+++
@@ -1,5 +0,0 @@
-import * as slim from "./slim_modules.ts";
-declare global {
-    var SlimConsole:slim.colorconsole.SlimColorConsole;
-    interface Window { SlimConsole:slim.colorconsole.SlimColorConsole; }
-}
--- slim_modules.ts
+++
@@ -1,4 +0,0 @@
-import * as colorconsole from "http://192.168.122.59/slim.console/index.ts";
-export {
-    colorconsole
-}
