f9f136f8c67d99e73492051d280e0765ebde7479
Jeff Greer <greergan@gmail.com>
2023-03-24 18:40:46 -0700

    generate again writes valid output

--- slim.generator/generate.ts
+++ slim.generator/generate.ts
@@ -1,15 +1,15 @@
 #!/usr/bin/env -S deno run --allow-env -r --check 
 //deno run --allow-env -r --check generate.ts -c http://192.168.122.59/models/website.json
 import * as slim from "./slim_modules.ts";
-import { check_config, explode_models, is_valid_output_namespace, parse_command_line, set_input_output } from "./index.ts";
+import { check_config, explode_models, is_valid_output_namespace, parse_command_line, set_input_output, write_output } from "./index.ts";
 try {
-    window.SlimConsole = new slim.colorconsole.SlimColorConsole(await slim.utilities.get_file_contents("http://192.168.122.59/models/console_colors.json"));
+    window.SlimConsole = new slim.colorconsole.SlimColorConsole(await slim.utilities.get_json_contents("http://192.168.122.59/models/console_colors.json"));
     SlimConsole.configurations.trace.stackTrace.suppress = true;
     console.info({message:"awaiting parse_command_line"}, Deno.args);
     const parsed_command_line:slim.types.iKeyValueAny = await parse_command_line(Deno.args);
     console.info({message:"awaiting get config_file"}, parsed_command_line.config_file);
-    const config:slim.types.iKeyValueAny|undefined = await slim.utilities.get_file_contents(parsed_command_line.config_file);
-    if(config === undefined) {
+    const config:slim.types.iKeyValueAny|undefined = await slim.utilities.get_json_contents(parsed_command_line.config_file);
+    if(!config) {
         SlimConsole.abort({message:"valid configuration file not found"});
     }
     if(!(await check_config(config!))) {
@@ -31,22 +31,22 @@ try {
     console.info({message:"Beginning page generation"});
     console.debug({message:"config.generate_ids",value:config!.generate_ids});
     console.debug({message:"parsed_command_line.generate_id",value:parsed_command_line.generate_id});
-    for(let page of config!.pages) {
+    for(const page of config!.pages) {
         let continue_processing = true;
-        if('generate_id' in page) {
-            if('generate_ids' in config!) {
+        if(page.hasOwnProperty('generate_id')) {
+            if(config!.hasOwnProperty('generate_ids')) {
                 if(!config!.generate_ids.includes(page.generate_id)) {
                     continue_processing = false;
                 }
             }
-            if('generate_id' in parsed_command_line && page.generate_id != parsed_command_line.generate_id) {
+            if(parsed_command_line.hasOwnProperty('generate_id') && page.generate_id != parsed_command_line.generate_id) {
                 continue_processing = false;
             }
         }
         if(continue_processing) {
-            console.info({message:"Generating output for",value:page.title}, `page.generate_id => ${page.generate_id}`);
+            console.info({message:"Generating output",value:"page"}, page.title, `page.generate_id => ${page.generate_id}`);
             let exploaded_models:slim.types.iKeyValueAny[] = [];
-            if(page.external_models !== undefined) {
+            if(page.hasOwnProperty('external_models')) {
                 if(!Array.isArray(page.external_models)) {
                     SlimConsole.abort({message:"page level property",value:"external_models not an Array"}, page.title);
                 }
@@ -55,9 +55,13 @@ try {
                     delete page.external_models;
                 }
             }
-            let model = slim.utilities.comingleSync([{title:""}, page, config!.site, ...exploaded_models], {depth:1});
-            console.debug({ message:"comingled model"}, model);
-            
+            let model = slim.utilities.comingleSync([{}, {page:page,site:config!.site}, ...exploaded_models]);
+            //console.debug({ message:"comingled model"}, model);
+            const input_output:slim.types.iKeyValueAny = await set_input_output(model.page, output_to!, namespace!);
+            const html_string:string = await view.render(model, input_output.input_file);
+            console.debug({ message:"rendered",value:"page size"}, html_string.length);
+            await write_output(input_output.output_file, html_string);
+            console.debug({ message:"wrote",value:"to output file"}, input_output.output_file);
         }
         else {
             console.debug({message:"ending page processing",value:"page.generate_id is excluded"}, page.generate_id);
@@ -65,15 +69,6 @@ try {
     }
 }
 catch(e) {
+    console.error(e.stack);
     SlimConsole.abort(e.stack);
 }
-/*
-        
-        model = {page:model};
-        model = await comingle(model, exploaded_models);
-        new trace({message:"Generating output for"}, model.page.title);
-        const input_output:KeyValueAny = await set_input_output(model.page, output_to, namespace);
-        new debug(input_output.input_file)
-        const results = await view.render(model, input_output.input_file);
-        await write_output(input_output.output_file, results);
-*/
