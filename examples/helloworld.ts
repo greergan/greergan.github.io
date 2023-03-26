29f9244df75d5bfd3b6676906b30aeb1660cf5f7
Jeff Greer <greergan@gmail.com>
2023-03-26 15:58:02 -0700

    simple Slim.View example working

--- examples/helloworld.ts
+++ examples/helloworld.ts
@@ -1,4 +1,4 @@
-import * as slim from "https://greergan.github.io/slim.view/slim_modules.ts";
-const view:slim.view.SlimView = new slim.view.SlimView("https://greergan.github.io");
-const html_view:string = view.render({page:{title:"fun"}, site:{copyright:{started_date:"2023",by:"Jeff Greer"}}}, "examples/helloworld.html");
+import { SlimView } from "https://greergan.github.io/slim.view/index.ts";
+const view:SlimView = new SlimView("https://greergan.github.io");
+const html_view:string = await view.render({page:{title:"fun"}, site:{copyright:{started_date:"2023",by:"Jeff Greer"}}}, "examples/helloworld.html");
 console.log(html_view);
